aa423d351d5e078d3e1a92684595cc465b638fae
suyeonkwoun94 <suyeonkwoun@gmail.com>
2020-08-16 15:17:44 +0900

    Updates

---
+++ precache-manifest.b5e1b54c83eb586c1368b0d05bcb41b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be6846dd314ed64768eda612c605532",
+    "url": "//suyeonkwoun.io/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a8297c6634559e9665c4",
+    "url": "//suyeonkwoun.io/movie_app_2020/static/css/main.7e29399e.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "//suyeonkwoun.io/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//suyeonkwoun.io/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8297c6634559e9665c4",
+    "url": "//suyeonkwoun.io/movie_app_2020/static/js/main.19f874aa.chunk.js"
+  },
+  {
+    "revision": "5247729777d5b463bc5c",
+    "url": "//suyeonkwoun.io/movie_app_2020/static/js/runtime-main.f25a17e7.js"
+  }
+]);
